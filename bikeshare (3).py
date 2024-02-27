0f57eca6b1d99af7201b58e11d4b8d4f8d529af3
Asma Al Mazroui <aalmazrouie@eand.com>
2024-02-27 10:38:27 +0400

    style : delete comment in time_stats(df)

--- bikeshare (3).py
+++ bikeshare (3).py
@@ -76,7 +76,6 @@ def load_data(city, month, day):
     return df
 
 def time_stats(df):
-    """Displays statistics on the most frequent times of travel."""
 
     print('\nCalculating The Most Frequent Times of Travel...\n')
     start_time = time.time()
@@ -93,8 +92,6 @@ def time_stats(df):
 
 
 def station_stats(df):
-    """Displays statistics on the most popular stations and trip."""
-
     print('\nCalculating The Most Popular Stations and Trip...\n')
     start_time = time.time()
 
